cc4dfbe6a921eb890fd9290325eed9b579b5a1f9
buxianshan <583425306@qq.com>
2022-03-19 18:18:01 +0800

    style: 注释掉log

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,7 @@
 export default ({router}) => {
     // 路由切换事件处理
     router.beforeEach((to, from, next) => {
-        console.log(`切换路由 from ${from.fullPath} to ${to.fullPath}`)
+        // console.log(`切换路由 from ${from.fullPath} to ${to.fullPath}`)
         // 触发百度的pv统计
         if (typeof _hmt != 'undefined') {
             if (to.path) {
