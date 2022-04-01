56e4f01e166c06b9de0312e4b2293e7b1292528b
buxianshan <583425306@qq.com>
2022-04-01 16:48:43 +0800

    feature: add markdwon container code-details

--- docs/.vuepress/plugins/index.js
+++ docs/.vuepress/plugins/index.js
@@ -16,6 +16,17 @@ module.exports = {
                 before: info => `<div class="theorem"><p class="title">${info}</p>`,
                 after: '</div>',
             },
-        ]
+        ],
+        [
+            'container',
+            {
+                type: 'code-details',
+                before: info => {
+                    let summary = info !== "CODE-DETAILS" ? info : '查看代码'
+                    return `<details class="code-details-block details"><summary>${summary}</summary>\n`
+                },
+                after: () => '</details>\n'
+            }
+        ],
     ]
 }
