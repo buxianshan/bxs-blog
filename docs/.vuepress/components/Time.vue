140e24a86b7e3b22c7ca1462fd9e1396cdd2f1f6
buxianshan <583425306@qq.com>
2023-01-01 01:07:13 +0800

    fix: md中嵌入js导致编译报错，改为使用vue组件mounted

---
+++ docs/.vuepress/components/Time.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="time"></div>
+</template>
+
+<script>
+export default {
+  name: "Time",
+  methods: {
+    refreshTime() {
+      let date = new Date();
+      let month = date.getMonth() + 1;
+      month = month > 9 ? month : `0${month}`;
+      let day = date.getDay() + 1;
+      day = day > 9 ? day : `0${day}`;
+      let time = date.getFullYear() + "-" + month + "-" + day + " " + date.toLocaleTimeString();
+      let element = document.getElementById("time");
+      element.innerHTML = time;
+    }
+  },
+  mounted() {
+    setInterval(this.refreshTime, 1000);
+  }
+}
+</script>
+
+<style scoped>
+#time {
+  font-size: 60px;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  height: 600px;
+  line-height: 600px;
+}
+</style>
