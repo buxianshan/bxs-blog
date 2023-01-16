0dd61aa6c1e6498ac429e93a9a988e048d7dc61b
buxianshan <583425306@qq.com>
2023-01-16 10:27:58 +0800

    feature: 添加一个用于自定义当前页面内容宽度的组件

---
+++ docs/.vuepress/components/ContentMaxWidth.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="reset-content-width"></div>
+</template>
+
+<script>
+export default {
+  name: "ContentMaxWidth",
+  props: {
+    maxWidth: String,
+  },
+  mounted() {
+    // 修改页面内容宽度
+    let page = document.getElementsByClassName("theme-default-content")[0]
+    page.setAttribute("style", `max-width: ${this.maxWidth}; margin: 0 auto; padding: 2em 2.5em;`)
+    page.classList.add("custom")
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
